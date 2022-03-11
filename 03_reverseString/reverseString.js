6a4e66828aa01595addf96430413ddc9d44f84b6
Brynn <durantbrynn@gmail.com>
2022-03-10 23:56:24 -0600

    Wrote program that splits inputted string to an array, reverses the array, and returns the reversed array as a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let userWord = string;
+    let originalSplit = string.split('');
+    return originalSplit.reverse().toString().replaceAll(',','');
 };
 
 // Do not edit below this line
