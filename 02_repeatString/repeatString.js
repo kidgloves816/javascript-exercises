d7c13c0d61066b593f9cb93e4d568db4ef879616
Brynn <durantbrynn@gmail.com>
2022-03-10 23:36:24 -0600

    wrote program to take string input, and output the string repeated a given number of times.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) 
+        return 'ERROR';
 
+    else {
+        let word = string;
+        string = '';
+        for (let i = 1; i <= num; i++){
+            string += word;
+        }
+
+    return string;
+    }
 };
 
 // Do not edit below this line
