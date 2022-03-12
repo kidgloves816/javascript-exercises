c11ef609e3c4892896c6005a6113688077b83c29
Brynn <durantbrynn@gmail.com>
2022-03-12 14:59:41 -0600

    Wrote algorithm to delete all values in an array, and tested the condition.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,13 @@
 const removeFromArray = function(userArray,indexToDelete) {
-    userArray.splice(indexToDelete-1,1);
-    return userArray;
-
+    let i = 1;
+    while (i < arguments.length) {
+        if (arguments[i] <= userArray.length && !isNaN(arguments[i])){
+            indexToDelete = arguments[i];
+            userArray.splice(indexToDelete-1,1,null);
+        }
+        i++;
+    }
+    return userArray.filter(Boolean);
 };
 
 // Do not edit below this line
