f2df70e16eec5700e89f0620dd1041fc7d3a71bd
Brynn <durantbrynn@gmail.com>
2022-03-14 15:29:14 -0500

    Added function to sum all digits between two numbers, and tested the function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let firstNumber = Math.min(num1,num2);
+    let secondNumber = Math.max(num1,num2);
 
+    let sum = 0;
+
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
+
+    else {
+    for (i = firstNumber;i <= secondNumber; i++){
+        sum += i;
+    }
+
+    return sum;
+}
 };
 
 // Do not edit below this line
